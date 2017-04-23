d9bd764035c4a2254dce2323661ea94f318220aa
Alexander Dietmüller <alexander@dietmueller.de>
2017-04-23 19:01:56 +0200

    Split up product list to use separated items

---
+++ src/components/product-list-item.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h3 v-on:click="$router.push({name: 'product', params:{id: product._id}})">
+    {{ product.title}}
+  </h3>
+  <small>
+    Von
+    <router-link :to="{name: 'products', query: {vendor: product.vendor}}">
+      {{ product.vendor }}
+    </router-link>
+    in der Kategorie
+    <router-link
+        :to="{name: 'products', query: {category: product.category}}">
+      {{ product.category }}
+    </router-link>
+  </small>
+  <p>{{ product.price }} €</p>
+  <button v-on:click="$api.addToCart(product._id)">
+    In den Warenkorb
+  </button>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['product']
+}
+</script>
--- src/components/product-list.vue
+++ src/components/product-list.vue
@@ -1,32 +1,16 @@
 <template>
 <ul>
   <li v-for="product in products">
-    <h3>
-      <router-link :to="{name: 'product', params:{id: product._id}}">
-        {{ product.title}}
-      </router-link>
-    </h3>
-    <small>
-      Von
-      <router-link :to="{name: 'products', query: {vendor: product.vendor}}">
-        {{ product.vendor }}
-      </router-link>
-      in der Kategorie
-      <router-link
-          :to="{name: 'products', query: {category: product.category}}">
-        {{ product.category }}
-      </router-link>
-    </small>
-    <p>{{ product.price }} €</p>
-    <button v-on:click="$api.addToCart(product._id)">
-      In den Warenkorb
-    </button>
+    <product-list-item :product='product'></product-list-item>
   </li>
 </ul>
 </template>
 
 <script>
+import ProductListItem from './product-list-item.vue'
+
 export default {
+  components: { ProductListItem },
   computed: {
     products: function () {
       return this.$api.get('products', this.$route.query)
